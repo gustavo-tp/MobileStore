6a108e5c90f5c3a6200b446a1b22a1c6c305f6fe
Gustavo Turra Perandré <gustavo1190.gt@gmail.com>
2023-10-19 16:57:13 -0300

    style: load fonts and change header style

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,37 @@
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_900Black,
+} from "@expo-google-fonts/roboto";
 import { Stack } from "expo-router/stack";
 import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_900Black,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <>
       <Stack
         screenOptions={{
           headerStyle: {
-            backgroundColor: "#f4511e",
+            backgroundColor: "#1F6E8C",
           },
           headerTintColor: "#fff",
           headerTitleStyle: {
-            fontWeight: "bold",
+            fontFamily: "Roboto_900Black",
           },
         }}
       />
-      <StatusBar backgroundColor="#f45" />
+      <StatusBar backgroundColor="#2E8A99" />
     </>
   );
 }
