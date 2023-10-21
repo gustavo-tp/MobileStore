96d9461e740e0202e6b6555f81452d2342deda2a
Gustavo Turra Perandré <gustavo1190.gt@gmail.com>
2023-10-21 12:25:10 -0300

    style: color palette

---
+++ src/config/colors.ts
@@ -0,0 +1,24 @@
+const Colors = {
+  primary: "#1F6E8C",
+  primaryVariant: "#2E8A99",
+  secondary: "#1976D2",
+
+  gold: "#FFB000",
+  red: "#D21312",
+  white: "#FFF",
+  black: "#000",
+
+  gray: {
+    100: "#F8F9FA",
+    200: "#E9ECEF",
+    300: "#DEE2E6",
+    400: "#CED4DA",
+    500: "#ADB5BD",
+    600: "#6C757D",
+    700: "#495057",
+    800: "#343A40",
+    900: "#212529",
+  },
+};
+
+export default Colors;
