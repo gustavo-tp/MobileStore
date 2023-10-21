f543f8ec973b998d8f806b33a2af2a6b2bb091d7
Gustavo Turra Perandré <gustavo1190.gt@gmail.com>
2023-10-21 14:21:14 -0300

    refactor: move products list into a separated comp

---
+++ src/components/ProductsList/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 12,
+    paddingHorizontal: 8,
+    rowGap: 8,
+    minWidth: "100%",
+  },
+});
+
+export default styles;
