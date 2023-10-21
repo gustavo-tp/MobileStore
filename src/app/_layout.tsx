0c01d8b017d97af70df6495ada604715ce2999af
Gustavo Turra Perandré <gustavo1190.gt@gmail.com>
2023-10-21 12:27:10 -0300

    feat: add cart provider, set colors from palette

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,9 @@ import {
 import { Stack } from "expo-router/stack";
 import { StatusBar } from "expo-status-bar";
 
+import Colors from "@/config/colors";
+import { CartProvider } from "@/hooks/cart";
+
 export default function Layout() {
   const [fontsLoaded, fontError] = useFonts({
     Roboto_400Regular,
@@ -20,18 +23,20 @@ export default function Layout() {
 
   return (
     <>
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#1F6E8C",
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontFamily: "Roboto_900Black",
-          },
-        }}
-      />
-      <StatusBar backgroundColor="#2E8A99" />
+      <StatusBar backgroundColor={Colors.primaryVariant} />
+      <CartProvider>
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: Colors.primary,
+            },
+            headerTintColor: Colors.white,
+            headerTitleStyle: {
+              fontFamily: "Roboto_900Black",
+            },
+          }}
+        />
+      </CartProvider>
     </>
   );
 }
